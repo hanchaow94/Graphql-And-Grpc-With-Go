6897dc42aa712bfbe77efbe888990da41dc779a0
Hanchao Wang <hanchao@cybertinolab.com>
2022-04-06 17:11:37 +0800

    adjust folder hierarchy

---
+++ gateway/graph/schema.resolvers.go
@@ -0,0 +1,32 @@
+package graph
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"context"
+	"example/gateway/graph/generated"
+	"example/gateway/graph/model"
+	"fmt"
+)
+
+func (r *mutationResolver) Operate(ctx context.Context, input model.Operation) (bool, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) Self(ctx context.Context, id string) (*model.UserProfile, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) Users(ctx context.Context, id *string) ([]*model.UserShowFile, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+// Mutation returns generated.MutationResolver implementation.
+func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
+
+// Query returns generated.QueryResolver implementation.
+func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
+
+type mutationResolver struct{ *Resolver }
+type queryResolver struct{ *Resolver }
